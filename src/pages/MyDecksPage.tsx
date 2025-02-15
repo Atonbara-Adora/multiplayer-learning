d3ae75fbccb61271c1ded2937ad15db482c92254
yongyetan1209 <yongyetan1209@gmail.com>
2025-02-15 18:29:09 -0500

    navigate back to home from my decks page

--- src/pages/MyDecksPage.tsx
+++ src/pages/MyDecksPage.tsx
@@ -1,13 +1,25 @@
 import { MyDeckCard } from "../components/myDecks";
 import { useDeckStore } from "../stores/DeckStore";
 import { useFlashCardStore } from "../stores/FlashcardStore";
+import { useNavigate } from "react-router-dom";
 
 const MyDecksPage = () => {
+    const navigate = useNavigate();
     const { clearStore } = useFlashCardStore();
     const { deck } = useDeckStore();
+
+    const navigateBack = () => {
+        navigate("/");
+    }
+
     return (
         <div className="flex flex-col py-4">
-            <h1 className="text-3xl text-center">My Decks</h1>
+            <div className="flex justify-center">
+                <button onClick={navigateBack} className="absolute left-4 top-4 text-2xl text-gray-800 hover:text-gray-600 hover:bg-gray-200">
+                    ←
+                </button>
+                <h1 className="text-3xl text-center">My Decks</h1>
+            </div>
 
             <main>
                 <div className="overflow-x-auto px-4">
