a2cf979ca39d5574b67acee6c3b6560ea509535e
yongyetan1209 <yongyetan1209@gmail.com>
2025-02-15 17:17:28 -0500

    refactor card store

--- src/stores/FlashcardStore.tsx
+++ src/stores/FlashcardStore.tsx
@@ -1,40 +1,39 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
+import { StateCreator } from 'zustand';
 
-type CardType = {
+export type CardType = {
     text: string;
     answers: string[];
     correctAnswer: string;
 };
 
-
-interface FlashCardStore {
+export interface FlashCardStore {
     classDeckName: string;
     questions: CardType[];
     setDeckName: (deckName: string) => void;
     createQuestion: () => void;
 }
 
+const flashCardSlice: StateCreator<FlashCardStore> = (set) => ({
+    classDeckName: "",
+    questions: [],
+    setDeckName: (deckName: string) => set({ classDeckName: deckName }),
+    createQuestion: () =>
+        set((state) => ({
+            questions: [
+                ...state.questions,
+                {
+                    text: "Question",
+                    answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+                    correctAnswer: "Answer 1",
+                },
+            ],
+        })),
+});
+
 export const useFlashCardStore = create<FlashCardStore>()(
-    persist(
-        (set) => ({
-            classDeckName: "",
-            questions: [],
-            setDeckName: (deckName: string) => set({ classDeckName: deckName }),
-            createQuestion: () =>
-                set((state) => ({
-                    questions: [
-                        ...state.questions,
-                        {
-                            text: "Question",
-                            answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-                            correctAnswer: "Answer 1",
-                        },
-                    ],
-                })),
-        }),
-        {
-            name: 'flashcard-store',
-        }
-    )
+    persist(flashCardSlice, {
+        name: 'flashcard-store', // unique name for the storage key
+    })
 );
