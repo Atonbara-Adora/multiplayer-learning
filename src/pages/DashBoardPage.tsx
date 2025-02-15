5bda6975dc5b44b894dc169b78223aeacfe4ea4a
yongyetan1209 <yongyetan1209@gmail.com>
2025-02-15 13:14:54 -0500

    add header

---
+++ src/pages/DashBoardPage.tsx
@@ -0,0 +1,13 @@
+import DashboardHeader from "../components/dashbaord/DashBoardHeader";
+
+const DashBoard = () => {
+    return (
+        <div>
+            <header>
+                <DashboardHeader />
+            </header>
+        </div>
+    );
+};
+
+export default DashBoard;
