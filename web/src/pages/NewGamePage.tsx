994641f384e1ccdecffad1a34034647708be98cf
yongyetan1209 <yongyetan1209@gmail.com>
2025-02-15 22:15:14 -0500

    move to web folder

--- web/src/pages/NewGamePage.tsx
+++ web/src/pages/NewGamePage.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { useDeckStore } from "../stores/DeckStore";
 import { useNavigate } from "react-router-dom";
 
 const NewGamePage = () => {
     const { deck } = useDeckStore();
     const navigate = useNavigate();
+
+    const [selectedDeck, setSelectedDeck] = useState('');
 
     const startGame = () => {
         console.log('Start Game');
@@ -15,7 +18,8 @@ const NewGamePage = () => {
             <h1 className="text-3xl">New Game</h1>
 
             <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn m-1">Click
+                <div tabIndex={0} role="button" className="btn m-1">
+                    {selectedDeck === '' ? 'Select Deck' : selectedDeck}
                     <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7" />
                     </svg>
@@ -23,7 +27,7 @@ const NewGamePage = () => {
                 </div>
                 <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
                     {deck.map((deck, index) => (
-                        <li key={index}><a>{deck.classDeckName}</a></li>
+                        <li key={index} onClick={() => setSelectedDeck(deck.classDeckName)}><a>{deck.classDeckName}</a></li>
                     ))
                     }
                 </ul>
@@ -38,7 +42,7 @@ const NewGamePage = () => {
                     <div className="flex-shrink-0 bg-gray-200 rounded-xl w-36 h-36 flex items-center justify-center">
                         <span className="text-black text-8xl">+</span>
                     </div>
-                    <span className="text-white mt-2">Add Player</span>
+                    <span className="text-black mt-2">Add Player</span>
                 </div>
             </div>
 
@@ -51,7 +55,7 @@ const PlayerGrid = ({ name }: { name: string }) => {
     return (
         <div className="flex flex-col items-center">
             <div className="flex-shrink-0 p-4 bg-gray-200 rounded-2xl w-36 h-36 flex items-center justify-center"></div>
-            <span className="text-white mt-2">{name}</span>
+            <span className="text-black mt-2">{name}</span>
         </div>
     );
 };
